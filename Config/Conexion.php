015b8ed1d81887f55f797426b4c8d457962e9c35
wilgen jareth vasquez torrez <est.wvasquezt0201@uml.edu.ni>
2025-11-09 14:31:14 -0600

    Credenciales actualizadas para base de datos en linea

--- Config/Conexion.php
+++ Config/Conexion.php
@@ -1,9 +1,15 @@
 <?php
+  // Credenciales de la base de datos local
+  // $host = "localhost";
+  // $user = "root";
+  // $pass = "wilgen12345";
+  // $db = "control_asistencia_completa_2";
 
-  $host = "localhost";
-  $user = "root";
-  $pass = "wilgen12345";
-  $db = "control_asistencia_completa_2";
+  // Credenciales de la base de datos remota en infinityfree
+  $host = "sql112.infinityfree.com";
+  $user = "rif0_40373614";
+  $pass = "uu626Ffw8LFU74";
+  $db = "if0_40373614_control_asistencia_completa_2";
 
   $conexion =new mysqli($host,$user,$pass,$db);
 
